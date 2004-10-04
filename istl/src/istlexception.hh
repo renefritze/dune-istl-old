323ba71b6921f7ba9a4a43c0eddf4c249aa491fa
peter <peter@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2004-10-04 15:15:39 +0000

    restructured matrix classes...
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@29 8c7282f0-4411-0410-8e4f-cdd3e049afb8

---
+++ istl/src/istlexception.hh
@@ -0,0 +1,23 @@
+#ifndef __DUNE_ISTLEXC_HH__
+#define __DUNE_ISTLEXC_HH__
+
+#include <stdlib.h>
+
+#include "../../common/exceptions.hh"
+
+namespace Dune {
+   
+    /** @defgroup ISTL Iterative Solvers Template Library
+		@addtogroup ISTL
+		@{
+     */
+
+  //! derive error class from the base class in common
+  class ISTLError : public Exception {};
+
+ 
+  /** @} end documentation */
+
+} // end namespace
+
+#endif
---
+++ istl/src/vbcrsmatrix.hh
@@ -0,0 +1,29 @@
+#ifndef __DUNE_VBCRSMATRIX_HH__
+#define __DUNE_VBCRSMATRIX_HH__
+
+#include<math.h>
+#include<complex>
+
+#include "istlexection.hh"
+#include "allocator.hh"
+
+/*! \file __FILE__
+
+  This file implements a vector constructed from a given type
+  representing a field and a compile-time given size.
+*/
+
+namespace Dune {
+   
+    /** @defgroup ISTL Iterative Solvers Template Library
+		@addtogroup ISTL
+		@{
+     */
+
+
+
+  /** @} end documentation */
+
+} // end namespace
+
+#endif
