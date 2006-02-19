c59b08de5f03bc9080f8cd936f8e293c2cd1befc
mblatt <mblatt@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2006-02-19 00:23:11 +0000

    Modified test problem.
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@525 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- istl/paamg/test/parallelamgtest.cc
+++ istl/paamg/test/parallelamgtest.cc
@@ -109,7 +109,7 @@ int main(int argc, char** argv)
   b=0;
   x=100;
   
-  setBoundary(x, N, comm.indexSet());
+  setBoundary(x, b, N, comm.indexSet());
 
   Vector b1=b, x1=x;
 
@@ -136,7 +136,7 @@ int main(int argc, char** argv)
   
   SmootherArgs smootherArgs;
     
-  smootherArgs.iterations = 1;
+  smootherArgs.iterations = 2;
   
   
   Criterion criterion(15,coarsenTarget);
