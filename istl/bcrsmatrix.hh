717c9b77377f32531c28e48ed9d4a7f19d758df5
mblatt <mblatt@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2006-11-03 15:21:47 +0000

    nnz has to be optional in allocate!
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@667 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- istl/bcrsmatrix.hh
+++ istl/bcrsmatrix.hh
@@ -1380,7 +1380,7 @@ namespace Dune {
      * @param nnz The number of nonzero entries the matrix should hold (if omitted 
      * defaults to 0).
      */
-    void allocate(size_type rows, size_type columns, size_type nnz_)
+    void allocate(size_type rows, size_type columns, size_type nnz_=0)
     {
       // Store size
       n = rows;
