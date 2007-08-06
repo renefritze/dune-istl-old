e725a08feb0ec9e40289f2dd604a3a61acba4abb
mblatt <mblatt@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2007-08-06 09:17:48 +0000

    Add method for querying the number of nonzeros. (Needed for superlu).
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@789 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- istl/bcrsmatrix.hh
+++ istl/bcrsmatrix.hh
@@ -1143,6 +1143,12 @@ namespace Dune {
 	  return m;
 	}
 
+    //! number of nonzeros
+    size_type Nnz() const
+    {
+      return nnz;
+    }
+    
       /** \brief row dimension of block r
           \bug Does not count empty rows (FlySpray #7)
       */
