ba8950c1d5477448ecb4dae85e54d073f8ff2dc6
mblatt <mblatt@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2008-02-20 13:19:00 +0000

    removed memory leaks.
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@862 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- istl/pardiso.hh
+++ istl/pardiso.hh
@@ -186,6 +186,9 @@ namespace Dune {
         F77_FUNC(pardiso) (pt_, &maxfct_, &mnum_, &mtype_, &phase,
     		       &n_, &ddum, ia_, ja_, &idum, &nrhs_,
     		       iparm_, &msglvl_, &ddum, &ddum, &error_);
+	delete[] a_;
+	delete[] ia_;
+	delete[] ja_;
 #endif
     }
   
