05148395df11d8983604b0ea89d01f2f2c8ac8b3
mblatt <mblatt@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2009-10-01 12:00:24 +0000

    Make sequential amg working again. Fixes flyspray #613
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@1089 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- istl/paamg/pinfo.hh
+++ istl/paamg/pinfo.hh
@@ -26,6 +26,7 @@ namespace Dune
     public:
       typedef CollectiveCommunication<void*> MPICommunicator;
       typedef EmptySet<int> CopyFlags;
+      typedef AllSet<int> OwnerSet;
       
       enum{
 	category = SolverCategory::sequential
