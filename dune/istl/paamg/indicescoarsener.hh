f5e45ed2bee54efed03161229418d7f8abc8f4d5
mblatt <mblatt@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2009-12-04 16:10:03 +0000

    Reverting patch 1138 which broke parallel amg. Martin is right, one should run make check _before_ committing.
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@1141 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- dune/istl/paamg/indicescoarsener.hh
+++ dune/istl/paamg/indicescoarsener.hh
@@ -30,7 +30,8 @@ namespace Dune
 
     template<typename T, typename E>
     class IndicesCoarsener
-    {    };
+    {
+    };
 
     
 #if HAVE_MPI
@@ -113,10 +114,11 @@ namespace Dune
 	{
 	  AggregateRenumberer<G>::operator()(edge);
 	  const IndexPair* pair= lookup_.pair(edge.target());
-	  assert(pair!=static_cast<const IndexPair*>(0));
-	  globalIndex(pair->global());
-	  attribute(pair->local().attribute());
-	  isPublic(pair->local().isPublic());
+	  if(pair!=0){
+	    globalIndex(pair->global());
+	    attribute(pair->local().attribute());
+	    isPublic(pair->local().isPublic());
+	  }
 	}
 	
 	Vertex operator()(const GlobalIndex& global)
@@ -159,7 +161,7 @@ namespace Dune
 	
 	void globalIndex(const GlobalIndex& global)
 	{
-	  globalIndex_ = std::min(global,globalIndex_);
+	  globalIndex_ = global;
 	}
 	
       private:
@@ -264,8 +266,7 @@ namespace Dune
 	    const IndexPair* pair= lookup.pair(*index);
 	      
 	    renumberer.reset();
-	    assert(pair!=0);
-	    if(!ExcludedAttributes::contains(pair->local().attribute())){
+	    if(pair!=0 && !ExcludedAttributes::contains(pair->local().attribute())){
 	      renumberer.attribute(pair->local().attribute());
 	      renumberer.isPublic(pair->local().isPublic());
 	      renumberer.globalIndex(pair->global());
@@ -277,12 +278,13 @@ namespace Dune
 	    
 	    typedef typename GlobalLookupIndexSet::IndexPair::GlobalIndex GlobalIndex;
 	    
-	    assert(renumberer.globalIndex()!=std::numeric_limits<GlobalIndex>::max());
+	    if(renumberer.globalIndex()!=std::numeric_limits<GlobalIndex>::max()){
+	      //std::cout <<" Adding global="<< renumberer.globalIndex()<<" local="<<static_cast<std::size_t>(renumberer)<<std::endl;
+	      coarseIndices.add(renumberer.globalIndex(), 
+				LocalIndex(renumberer, renumberer.attribute(), 
+					   renumberer.isPublic()));
+	    }
 	    
-	    coarseIndices.add(renumberer.globalIndex(), 
-			      LocalIndex(renumberer, renumberer.attribute(), 
-					 renumberer.isPublic()));
-	    	    
 	    aggregates[*index] = renumberer;
 	    ++renumberer;
 	  }
