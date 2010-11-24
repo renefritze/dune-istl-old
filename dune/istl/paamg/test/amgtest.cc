141ca1406746e64e19396eb33a4cf9c704098fae
sander <sander@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2010-11-24 17:37:39 +0000

    Fix include paths: the headers of the remote index sets have been moved to dune-common
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@1399 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- dune/istl/paamg/test/amgtest.cc
+++ dune/istl/paamg/test/amgtest.cc
@@ -17,7 +17,7 @@ namespace std
 #include<dune/common/timer.hh>
 #include<dune/istl/paamg/amg.hh>
 #include<dune/istl/paamg/pinfo.hh>
-#include<dune/istl/indexset.hh>
+#include<dune/common/parallel/indexset.hh>
 #include<dune/istl/solvers.hh>
 #include<dune/common/collectivecommunication.hh>
 #include<cstdlib>
