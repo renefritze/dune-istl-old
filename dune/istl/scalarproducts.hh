c464ee78f1a1a4a7365e2c909f0884955ed1de78
mblatt <mblatt@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2010-11-30 10:00:02 +0000

    revert patches 1408 and 1404. *sigh*
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@1410 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- dune/istl/scalarproducts.hh
+++ dune/istl/scalarproducts.hh
@@ -9,7 +9,6 @@
 
 #include"solvercategory.hh"
 
-#include<dune/common/ftraits.hh>
 
 namespace Dune {
   /**
@@ -54,8 +53,7 @@ sequential case are provided.
 	/*! \brief Norm of a right-hand side vector. 
 	  The vector must be consistent on the interior+border partition
 	 */
-	virtual typename FieldTraits<X>::real_type
-        norm (const X& x) = 0;
+	virtual double norm (const X& x) = 0;
 
 
 	//! every abstract base class has a virtual destructor
@@ -113,8 +111,7 @@ sequential case are provided.
 	/*! \brief Norm of a right-hand side vector. 
 	  The vector must be consistent on the interior+border partition
 	 */
-	virtual typename FieldTraits<X>::real_type
-        norm (const X& x)
+	virtual double norm (const X& x)
 	{
 	  return x.two_norm();
 	}
