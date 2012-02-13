59fb6cec018dff88a67366e64cb1a8079b7cc286
gruenich <gruenich@8c7282f0-4411-0410-8e4f-cdd3e049afb8>
2012-02-13 08:04:34 +0000

    Fix support for SuperLU 4.3.
    git-svn-id: https://svn.dune-project.org/svn/dune-istl/trunk@1529 8c7282f0-4411-0410-8e4f-cdd3e049afb8

--- dune/istl/superlu.hh
+++ dune/istl/superlu.hh
@@ -348,11 +348,11 @@ namespace Dune
     
     double def0=d.two_norm();
     */
-    #if SUPERLU_MIN_VERSION_4_3
+#ifdef SUPERLU_MIN_VERSION_4_3
     options.IterRefine=SLU_DOUBLE;
-    #else
+#else
     options.IterRefine=DOUBLE;
-    #endif
+#endif
     
     dgssvx(&options, &static_cast<SuperMatrix&>(mat), perm_c, perm_r, etree, &equed, R, C,
            &L, &U, work, lwork, &B, &X, &rpg, &rcond, &ferr, &berr,
@@ -417,11 +417,11 @@ namespace Dune
     /* Initialize the statistics variables. */
     StatInit(&stat);
 
-    #if SUPERLU_MIN_VERSION_4_3
+#ifdef SUPERLU_MIN_VERSION_4_3
     options.IterRefine=SLU_DOUBLE;
-    #else
+#else
     options.IterRefine=DOUBLE;
-    #endif
+#endif
     
     dgssvx(&options, &static_cast<SuperMatrix&>(mat), perm_c, perm_r, etree, &equed, R, C,
            &L, &U, work, lwork, &B, &X, &rpg, &rcond, &ferr, &berr,
@@ -448,5 +448,5 @@ namespace Dune
   /** @} */
 }
 
-#endif
-#endif
+#endif // HAVE_SUPERLU
+#endif // DUNE_SUPERLU_HH
